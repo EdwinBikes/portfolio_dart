10a2185a1901809e83b6d7ed878824fad894377d
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2022-10-19 13:23:21 -0500

    Update objetos.dart

--- ejemplo_objetos/objetos.dart
+++ ejemplo_objetos/objetos.dart
@@ -6,23 +6,23 @@ void main(){
   estatura: 170,
  ];
 
-let nombre2 = {
+var nombre2 = {
   nombre: "Oscar",
   apellido: "Gutierrez",
   edad: 20,
   estatura: 160,
 };
 
-let nombre3 = {
+var nombre3 = {
   nombre: "Yir",
   apellido: "Orozco",
   edad: 26,
   estatura: 150,
 };
 
-let personas = [nombre1, nombre2, nombre3];
+var personas = [nombre1, nombre2, nombre3];
  console.log(personas);
-  for (let i = 0; i < personas.length; i++){
+  for (var i = 0; i < personas.length; i++){
     let persona = personas[i];
      console.log(persona.nombre + persona.edad);
   }
